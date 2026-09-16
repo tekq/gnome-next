272e33253eb430118b6f99285feff13b16380a09
asmx2 <asmx2@noreply.codeberg.org>
2026-09-17 02:35:18 +0300

    fix hash

--- overlay.nix
+++ overlay.nix
@@ -79,7 +79,7 @@ in
       version = gnomeVersion;
       src = final.fetchurl {
         url = "mirror://gnome/sources/gnome-control-center/51/gnome-control-center-${gnomeVersion}.tar.xz";
-        hash = "sha256-Bd2wWpu7vD2AM2sEkh5VoPnU3TZ5fvjcDIHRjt58edc=";
+        hash = "sha256-yMpbagrkT9DWSqxao7eag8JEUJWxWanRPRycwXKsT7k=";
       };
       # patches = [ ];
     });
