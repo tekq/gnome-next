37f4c8354204841510e28ebf0b23d1eac64c725e
asmx2 <asmx2@noreply.codeberg.org>
2026-09-17 00:16:27 +0300

    drop second potch

--- overlay.nix
+++ overlay.nix
@@ -25,6 +25,7 @@ in
         hash = "sha256-XSjzriJWkkKPyvuWUA1nPzQyi2mLhpYMnBRg0LHZg7M=";
       };
       outputs = ["out" "dev" "man"];
+      patches = final.lib.take 1 old.patches;
       mesonFlags =
         final.lib.filter
         (flag: !(final.lib.hasPrefix "-Degl_device=" flag || final.lib.hasPrefix "-Dwayland_eglstream=" flag))
