013cd8087a01d2ae1c73ca5905063d649cb93fd5
asmx2 <asmx2@noreply.codeberg.org>
2026-09-17 17:23:42 +0300

    Add cmake

--- overlay.nix
+++ overlay.nix
@@ -117,6 +117,7 @@ in
         url = "mirror://gnome/sources/glibmm/2.90/glibmm-2.90.0.tar.xz";
         hash = "sha256-4u+kVkPxa5/qLWKZ8vQD1nLq6s3fD/f4CU4a+bD1mAs=";
       };
+      buildInputs = old.buildInputs ++ [final.cmake];
     });
 
     # WARNING: see the note at the top of this file before touching anything
